da362440de3cf3d1c49e41ba03688bce83433ec1
Pandu-5 <118616769+Pandu-5@users.noreply.github.com>
2023-04-07 23:17:21 +0530

    Add files via upload

---
+++ js/interview.js
@@ -0,0 +1,28 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activee");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activees", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " activees";
+}
